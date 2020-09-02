82ab0698454ca1b7f0016047834fc80f05200f95
Matt Calvert <mcalvert@metrostarsystems.com>
2020-09-02 11:41:59 -0400

    setting menu link as closed when another one is opened

--- js/Components/mega_menu.js
+++ js/Components/mega_menu.js
@@ -1,25 +1,28 @@
 window.addEventListener("load", function () {
   const sideMenuLinks = document.querySelectorAll(".sidemenu-link");
+  const activeClass = "active";
 
   sideMenuLinks.forEach((link) => {
     link.addEventListener("click", (event) => {
       const target = event.target;
       const linksSection = target.getAttribute("data-mega-menu-section");
       const activeContainers = document.querySelectorAll(
-        ".nav-mega-menu-links-container.active"
+        `.nav-mega-menu-links-container.${activeClass}`
       );
-      const activeLinks = document.querySelectorAll(".sidemenu-link.active");
+      const activeLinks = document.querySelectorAll(
+        `.sidemenu-link.${activeClass}`
+      );
 
       activeContainers.forEach((activeContainer) => {
-        activeContainer.classList.remove("active");
+        activeContainer.classList.remove(activeClass);
       });
       activeLinks.forEach((activeLink) => {
-        activeLink.classList.remove("active");
+        activeLink.classList.remove(activeClass);
       });
 
-      target.classList.add("active");
+      target.classList.add(activeClass);
       const openSection = document.getElementById(linksSection);
-      openSection.classList.add("active");
+      openSection.classList.add(activeClass);
     });
   });
 });
--- js/Components/menu.js
+++ js/Components/menu.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function(){
+window.addEventListener("load", function () {
     const helper = (element, className) => {
         if (!!element) {
             if (element.classList.contains(className)) {
@@ -7,79 +7,87 @@ window.addEventListener('load', function(){
                 return helper(element.parentElement, className);
             }
         }
-    }
+    };
 
-    let megamenuButtons = document.querySelectorAll('.accordion-button.nav-link');
-    if(megamenuButtons) {
-        [...megamenuButtons].map( elm => {
-
+    let submenuButtons = document.querySelectorAll(
+        ".accordion-button.nav-link"
+    );
+    if (submenuButtons) {
+        [...submenuButtons].map((elm) => {
             let submenu = elm.nextElementSibling;
 
-            elm.addEventListener('click', (evt) => {
+            elm.addEventListener("click", (evt) => {
                 evt.preventDefault();
-                let elmExpanded = elm.getAttribute('aria-expanded');
-                let submenuHidden = submenu.getAttribute('aria-hidden');
+                let elmExpanded = elm.getAttribute("aria-expanded");
+                let submenuHidden = submenu.getAttribute("aria-hidden");
 
                 //remove existing submenus
-                let openSubmenus = document.querySelectorAll('.submenu-item-active');
-                [...openSubmenus].map( el => {
-                    el.classList.remove('submenu-item-active')
+                let openSubmenus = document.querySelectorAll(
+                    ".submenu-item-active"
+                );
+                [...openSubmenus].map((el) => {
+                    el.setAttribute("aria-expanded", false);
+                    el.classList.remove("submenu-item-active");
                 });
 
                 // reveal appropriate submenu
-                if (elmExpanded == 'false') {
-                    elm.classList.add('submenu-item-active');
-                    elm.setAttribute('aria-expanded', 'true');
-                    submenu.setAttribute('aria-hidden','false');
+                if (elmExpanded == "false") {
+                    elm.classList.add("submenu-item-active");
+                    elm.setAttribute("aria-expanded", "true");
+                    submenu.setAttribute("aria-hidden", "false");
                 } else {
-                    elm.classList.remove('submenu-item-active');
-                    elm.setAttribute('aria-expanded', 'false');
-                    submenu.setAttribute('aria-hidden','true');
+                    elm.classList.remove("submenu-item-active");
+                    elm.setAttribute("aria-expanded", "false");
+                    submenu.setAttribute("aria-hidden", "true");
                 }
-            })
+            });
 
-            elm.addEventListener('keyup', event => {
+            elm.addEventListener("keyup", (event) => {
                 if (event.key == " " || event.key == "Enter") {
                     event.preventDefault();
                     elm.click();
                 }
-            })
-        })
+            });
+        });
     }
 
-    let submenuItems = document.querySelectorAll('.nav-submenu > .has-submenu');
-    if(submenuItems) {
-        [...submenuItems].map( elm => {
-            elm.addEventListener('click', (evt) => {
+    let submenuItems = document.querySelectorAll(".nav-submenu > .has-submenu");
+    if (submenuItems) {
+        [...submenuItems].map((elm) => {
+            elm.addEventListener("click", (evt) => {
                 evt.preventDefault();
-                [...submenuItems].map( elm2 => {
-                    elm2.classList.remove('submenu-item-active');
-                })
-                elm.classList.toggle('submenu-item-active');
-            })
-        })
+                [...submenuItems].map((elm2) => {
+                    elm2.classList.remove("submenu-item-active");
+                });
+                elm.classList.toggle("submenu-item-active");
+            });
+        });
     }
 
-    let govBannerButtons = document.querySelectorAll('button.usa-accordion-button.usa-banner-button');
+    let govBannerButtons = document.querySelectorAll(
+        "button.usa-accordion-button.usa-banner-button"
+    );
     if (govBannerButtons) {
-        [...govBannerButtons].map( item => {
-            item.addEventListener('click', (evt) => {
+        [...govBannerButtons].map((item) => {
+            item.addEventListener("click", (evt) => {
                 evt.preventDefault();
                 evt.stopPropagation();
 
-                let ariaVal = item.getAttribute('aria-expanded');
-                let banner = helper(item.parentElement, 'usa-accordion').querySelector('div[id^="gov-banner"]');
-                let newVal = ariaVal == 'true' ? 'false': 'true';
-                let hideContainer = newVal == 'false' ? 'true': 'false';
+                let ariaVal = item.getAttribute("aria-expanded");
+                let banner = helper(
+                    item.parentElement,
+                    "usa-accordion"
+                ).querySelector('div[id^="gov-banner"]');
+                let newVal = ariaVal == "true" ? "false" : "true";
+                let hideContainer = newVal == "false" ? "true" : "false";
 
-                helper(item.parentElement, 'usa-accordion')
-                    .querySelector('.usa-banner-header')
-                    .classList
-                    .toggle('usa-banner-header-expanded');
+                helper(item.parentElement, "usa-accordion")
+                    .querySelector(".usa-banner-header")
+                    .classList.toggle("usa-banner-header-expanded");
 
-                item.setAttribute('aria-expanded', newVal);
-                banner.setAttribute('aria-hidden', hideContainer);
-            })
-        })
+                item.setAttribute("aria-expanded", newVal);
+                banner.setAttribute("aria-hidden", hideContainer);
+            });
+        });
     }
-})
+});
