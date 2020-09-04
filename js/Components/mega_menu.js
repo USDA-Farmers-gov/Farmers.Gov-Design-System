f0bda8c7fd4e234c56fd320db761005b18eb71e6
Matt Calvert <mcalvert@metrostarsystems.com>
2020-09-04 09:21:06 -0400

    added functionality to choose first link/section when switching between megamenus

--- js/Components/mega_menu.js
+++ js/Components/mega_menu.js
@@ -1,4 +1,47 @@
 window.addEventListener("load", function () {
+  const main = document.querySelector(".primary-nav-wrap.megamenu");
+  if (!!main) {
+    mainNav(main);
+    sideMenu();
+  }
+});
+
+function mainNav(main) {
+  const links = main.querySelectorAll(".nav-link");
+  const activeClass = "active";
+
+  links.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      const megaMenuId = event.currentTarget.getAttribute("aria-controls");
+      const activePanel = document.querySelector(
+        `.nav-mega-menu[aria-controls=${megaMenuId}]`
+      );
+
+      const allPanels = document.querySelectorAll(
+        ".nav-mega-menu-links-container"
+      );
+      const sideMenuLinks = activePanel.querySelectorAll(".sidemenu li a");
+
+      allPanels.forEach((panel) => {
+        panel.classList.remove(activeClass);
+      });
+
+      activePanel
+        .querySelector(".nav-mega-menu-links-container")
+        .classList.add(activeClass);
+
+      if (sideMenuLinks) {
+        sideMenuLinks.forEach((link, index) => {
+          index === 0
+            ? link.classList.add(activeClass)
+            : link.classList.remove(activeClass);
+        });
+      }
+    });
+  });
+}
+
+function sideMenu() {
   const sideMenuLinks = document.querySelectorAll(".sidemenu-link");
   const activeClass = "active";
 
@@ -27,4 +70,4 @@ window.addEventListener("load", function () {
       });
     });
   }
-});
+}
